f8bff7947ef6cf6210b24b1f6cd76b358133d86a
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2024-05-13 15:52:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,11 @@ warehouse_name = "COMPUTE_H"
 session.execute(f"USE WAREHOUSE {warehouse_name}")
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.my_dataframe(pd_df)
 st.stop()
 
 #try:
